e95f41f46828dbac41c9fa9f412f2db94b3bcb73
Madmin27 <admin@nizip.com>
2024-04-02 00:53:10 +0300

    Update Token.js

--- test/Token.js
+++ test/Token.js
@@ -1,12 +1,24 @@
+// test/Token.js
+
 const { expect } = require("chai");
 
 describe("Token contract", function () {
-  it("Deployment should assign the total supply of tokens to the owner", async function () {
-    const [owner] = await ethers.getSigners();
+  let Token;
+  let hardhatToken;
+  let owner;
 
-    const hardhatToken = await ethers.deployContract("Token");
+  beforeEach(async function () {
+    Token = await ethers.getContractFactory("Token");
+    [owner] = await ethers.getSigners();
+    hardhatToken = await Token.deploy();
+  });
 
-    const ownerBalance = await hardhatToken.balanceOf(owner.address);
-    expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
+  describe("Deployment", function () {
+    it("should assign the total supply of tokens to the owner", async function () {
+      const ownerBalance = await hardhatToken.balanceOf(owner.address);
+      const totalSupply = await hardhatToken.totalSupply();
+      // Use the BigNumber `eq` method to compare values of BigNumbers
+      expect(ownerBalance.eq(totalSupply)).to.be.true;
+    });
   });
 });
